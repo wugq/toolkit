6b6721d8145839ea066381ac68d300d00cd89d95
Wu Guoquan <39115529@qq.com>
2022-03-03 12:14:36 +0800

    refactoring password

--- cmd/password.go
+++ cmd/password.go
@@ -3,34 +3,17 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"math/rand"
-	"os"
-	"strings"
 	"toolkit/runner/passwordRunner"
 )
 
 var passwordFlag passwordRunner.PasswordFlag
-
-var (
-	lowerCharSet  = "abcdedfghijklmnopqrst"
-	upperCharSet  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	symbolCharSet = "!@#$%&*"
-	numberSet     = "0123456789"
-)
 
 var passwordCmd = &cobra.Command{
 	Use:   "password LENGTH",
 	Short: "Generate random password.",
 	Long:  `Generate random password.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) > 1 {
-			err := cmd.Help()
-			if err != nil {
-				return
-			}
-			os.Exit(0)
-		}
-		runPassword(args)
+		runPassword()
 	},
 }
 
@@ -44,59 +27,9 @@ func init() {
 	passwordCmd.Flags().IntVarP(&passwordFlag.Size, "length", "L", 8, "password length")
 }
 
-func runPassword(args []string) {
+func runPassword() {
 	passwordFlag = passwordRunner.UpdateFlag(passwordFlag)
 
-	fmt.Println(GeneratePassword(passwordFlag))
-
-}
-
-func MakeAllCharSet(flag passwordRunner.PasswordFlag) string {
-	var allCharsetBuilder strings.Builder
-	if flag.UseLowercase {
-		allCharsetBuilder.WriteString(lowerCharSet)
-	}
-	if flag.UseUppercase {
-		allCharsetBuilder.WriteString(upperCharSet)
-	}
-	if flag.UseSymbol {
-		allCharsetBuilder.WriteString(symbolCharSet)
-	}
-	if flag.UseNumber {
-		allCharsetBuilder.WriteString(numberSet)
-	}
-	return allCharsetBuilder.String()
-}
-
-func GeneratePassword(flag passwordRunner.PasswordFlag) string {
-	var password strings.Builder
-	var allCharset = MakeAllCharSet(flag)
-
-	if flag.UseLowercase {
-		random := rand.Intn(len(lowerCharSet))
-		password.WriteString(string(lowerCharSet[random]))
-	}
-	if flag.UseUppercase {
-		random := rand.Intn(len(upperCharSet))
-		password.WriteString(string(upperCharSet[random]))
-	}
-	if flag.UseSymbol {
-		random := rand.Intn(len(symbolCharSet))
-		password.WriteString(string(symbolCharSet[random]))
-	}
-	if flag.UseNumber {
-		random := rand.Intn(len(numberSet))
-		password.WriteString(string(numberSet[random]))
-	}
-
-	remainingLength := flag.Size - password.Len()
-	for i := 0; i < remainingLength; i++ {
-		random := rand.Intn(len(allCharset))
-		password.WriteString(string(allCharset[random]))
-	}
-	inRune := []rune(password.String())
-	rand.Shuffle(len(inRune), func(i, j int) {
-		inRune[i], inRune[j] = inRune[j], inRune[i]
-	})
-	return string(inRune)
+	password := passwordRunner.GeneratePassword(passwordFlag)
+	fmt.Println(password)
 }
--- runner/passwordRunner/index.go
+++ runner/passwordRunner/index.go
@@ -1,5 +1,10 @@
 package passwordRunner
 
+import (
+	"math/rand"
+	"strings"
+)
+
 type PasswordFlag struct {
 	UseUppercase bool
 	UseLowercase bool
@@ -23,4 +28,54 @@ func UpdateFlag(flag PasswordFlag) PasswordFlag {
 		flag.UseSymbol = true
 	}
 	return flag
+}
+
+func MakeAllCharSet(flag PasswordFlag) string {
+	var allCharsetBuilder strings.Builder
+	if flag.UseLowercase {
+		allCharsetBuilder.WriteString(lowerCharSet)
+	}
+	if flag.UseUppercase {
+		allCharsetBuilder.WriteString(upperCharSet)
+	}
+	if flag.UseSymbol {
+		allCharsetBuilder.WriteString(symbolCharSet)
+	}
+	if flag.UseNumber {
+		allCharsetBuilder.WriteString(numberSet)
+	}
+	return allCharsetBuilder.String()
+}
+
+func GeneratePassword(flag PasswordFlag) string {
+	var password strings.Builder
+	var allCharset = MakeAllCharSet(flag)
+
+	if flag.UseLowercase {
+		random := rand.Intn(len(lowerCharSet))
+		password.WriteString(string(lowerCharSet[random]))
+	}
+	if flag.UseUppercase {
+		random := rand.Intn(len(upperCharSet))
+		password.WriteString(string(upperCharSet[random]))
+	}
+	if flag.UseSymbol {
+		random := rand.Intn(len(symbolCharSet))
+		password.WriteString(string(symbolCharSet[random]))
+	}
+	if flag.UseNumber {
+		random := rand.Intn(len(numberSet))
+		password.WriteString(string(numberSet[random]))
+	}
+
+	remainingLength := flag.Size - password.Len()
+	for i := 0; i < remainingLength; i++ {
+		random := rand.Intn(len(allCharset))
+		password.WriteString(string(allCharset[random]))
+	}
+	inRune := []rune(password.String())
+	rand.Shuffle(len(inRune), func(i, j int) {
+		inRune[i], inRune[j] = inRune[j], inRune[i]
+	})
+	return string(inRune)
 }
