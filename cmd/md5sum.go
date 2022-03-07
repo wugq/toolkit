aeab54b4a09f8f8cfbc6d533846fb04b19cfb223
Wu Guoquan <39115529@qq.com>
2022-03-07 08:55:50 +0800

    update md5sum, moved text arg to -t

--- cmd/md5sum.go
+++ cmd/md5sum.go
@@ -8,13 +8,18 @@ import (
 	"toolkit/utils/fileUtil"
 )
 
+type Md5sumCmdData struct {
+	text string
+}
+
+var md5sumCmdData Md5sumCmdData
 var md5sumCmd = &cobra.Command{
 	Use:   "md5sum FILE",
 	Short: "check md5sum of a file or string.",
 	Long:  `check md5sum of a file or string.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			fmt.Println("Please specify a file or a string")
+		if len(args) > 1 {
+			fmt.Println("Please process one file at a time.")
 			os.Exit(1)
 		}
 		runMd5Sum(args)
@@ -23,24 +28,32 @@ var md5sumCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(md5sumCmd)
+
+	md5sumCmd.Flags().StringVarP(&md5sumCmdData.text, "text", "t", "", "specify a Text string")
 }
 
 func runMd5Sum(args []string) {
-	userInput := args[0]
-	isFile, _ := fileUtil.IsFile(userInput)
-	var checksum string
-	var err error
-	if isFile {
-		checksum, err = md5sumRunner.CheckFile(userInput)
+	if len(args) == 1 {
+		userInput := args[0]
+		isFile, err := fileUtil.IsFile(userInput)
+		if !isFile || err != nil {
+			fmt.Println("File is incorrect.")
+		}
+
+		checksum, err := md5sumRunner.CheckFile(userInput)
+		if err != nil {
+			fmt.Printf("error: %v", err)
+			return
+		}
+		fmt.Printf("%v %v", checksum, userInput)
+	} else if md5sumCmdData.text != "" {
+		checksum, err := md5sumRunner.CheckText(md5sumCmdData.text)
+		if err != nil {
+			fmt.Printf("error: %v", err)
+			return
+		}
+		fmt.Printf("%v %v", checksum, md5sumCmdData.text)
 	} else {
-		checksum, err = md5sumRunner.CheckText(userInput)
+		fmt.Println("Nothing to do.")
 	}
-
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
-	}
-
-	fmt.Printf("%v %v", checksum, userInput)
-
 }
